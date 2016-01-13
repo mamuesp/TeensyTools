6e845c571b429c92eaaa475dc2c5874c0a94f1cd
Manfred Mueller-Spaeth <fms1961@gmail.com>
2016-01-13 21:42:32 +0100

    First version

---
+++ node_defs.js
@@ -0,0 +1,904 @@
+var temp = {
+	"AudioAnalyzeFFT1024": {
+		"type": "AudioAnalyzeFFT1024",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"available": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioAnalyzeFFT256": {
+		"type": "AudioAnalyzeFFT256",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"available": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioAnalyzeNoteFrequency": {
+		"type": "AudioAnalyzeNoteFrequency",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"begin;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioAnalyzePeak": {
+		"type": "AudioAnalyzePeak",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"available": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioAnalyzePrint": {
+		"type": "AudioAnalyzePrint",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioAnalyzeToneDetect": {
+		"type": "AudioAnalyzeToneDetect",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"frequency": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"empty": {
+		"type": "empty",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioControl": {
+		"type": "AudioControl",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioControlAK4558": {
+		"type": "AudioControlAK4558",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"enable;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioControlSGTL5000": {
+		"type": "AudioControlSGTL5000",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"enable;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioControlWM8731master": {
+		"type": "AudioControlWM8731master",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioEffectBitcrusher": {
+		"type": "AudioEffectBitcrusher",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"bits": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioEffectChorus": {
+		"type": "AudioEffectChorus",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"begin;": {
+					"value": "0"
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioEffectDelayExternal": {
+		"type": "AudioEffectDelayExternal",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"delay": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioEffectDelay": {
+		"type": "AudioEffectDelay",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"delay": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioEffectEnvelope": {
+		"type": "AudioEffectEnvelope",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"noteOn;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioEffectFade": {
+		"type": "AudioEffectFade",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"fadeIn": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioEffectFlange": {
+		"type": "AudioEffectFlange",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"begin;": {
+					"value": "0"
+				},
+				"voices;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioEffectMultiply": {
+		"type": "AudioEffectMultiply",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioFilterBiquad": {
+		"type": "AudioFilterBiquad",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioFilterFIR": {
+		"type": "AudioFilterFIR",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"begin": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioFilterStateVariable:": {
+		"type": "AudioFilterStateVariable:",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"inputQueueArray)": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioInputAnalog": {
+		"type": "AudioInputAnalog",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioInputI2S": {
+		"type": "AudioInputI2S",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioMixer4": {
+		"type": "AudioMixer4",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioOutputAnalog": {
+		"type": "AudioOutputAnalog",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioOutputI2S": {
+		"type": "AudioOutputI2S",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioOutputPWM": {
+		"type": "AudioOutputPWM",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioOutputSPDIF": {
+		"type": "AudioOutputSPDIF",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioPlayMemory": {
+		"type": "AudioPlayMemory",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"play;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioPlayQueue": {
+		"type": "AudioPlayQueue",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"play;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioPlaySdRaw": {
+		"type": "AudioPlaySdRaw",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"begin;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioPlaySdWav": {
+		"type": "AudioPlaySdWav",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"begin;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioPlaySerialflashRaw": {
+		"type": "AudioPlaySerialflashRaw",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"begin;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioRecordQueue": {
+		"type": "AudioRecordQueue",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"begin": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioSynthWaveformDc": {
+		"type": "AudioSynthWaveformDc",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"amplitude": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioSynthNoisePink": {
+		"type": "AudioSynthNoisePink",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"amplitude": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioSynthWaveformSine": {
+		"type": "AudioSynthWaveformSine",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"frequency": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioSynthToneSweep": {
+		"type": "AudioSynthToneSweep",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"play;": {
+					"value": "0"
+				},
+				"update;": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioSynthWaveform": {
+		"type": "AudioSynthWaveform",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"frequency": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	},
+	"AudioSynthNoiseWhite": {
+		"type": "AudioSynthNoiseWhite",
+		"data": {
+			"defaults": {
+				"name": {
+					"value": "empty"
+				},
+				"getSource": {
+					"value": null
+				},
+				"amplitude": {
+					"value": "0"
+				}
+			},
+			"shortName": "empty",
+			"inputs": 0,
+			"outputs": 0,
+			"category": "empty",
+			"color": "#E6E0F8",
+			"icon": "arrow-in.png"
+		}
+	}
+};
--- scanHeader.js
+++ scanHeader.js
@@ -1,27 +1,205 @@
 /**
  * Created by mmspaeth on 07.12.15.
  */
+var glob = require('glob');
 var fs = require('fs');
-var cheerio = require('cheerio');
-var inputPath = (process.argv[2] + "/").replace("//", "/");
-var outputFile= process.argv[3]);
+var inputPath = (process.argv[2] + "/").replace("//", "/"); //.replace(/([ ])/g, '\\$1');
+var outputFile= process.argv[3];
 var tagAttr = process.argv[4];
 
-checkPath(outputPath, true);
+checkPath(inputPath, true);
 
-console.log("The file '" + inputFile + "' will be processed.");
-console.log("The path '" + outputPath + "' will be the output directory.");
+console.log("The file '" + outputFile + "' will be written.");
+console.log("The path '" + inputPath + "' will be the directory to scan.");
 
-fs.readFile(inputFile, function (err, data) {
-	if (err) throw err;
-	extractScripts(data);
-});
+scanFiles();
+
+function scanFiles() {
+
+	glob(inputPath + '*.h', function(err, files) {
+		var out = {};
+		if (err) throw err;
+		files.forEach(function(fileName) {
+			//fileName = fileName.replace(/([ ])/g, '\\$1');
+			console.log(fileName);
+			fs.readFile(""+fileName, "utf-8", function (err, data) {
+				if (err) {
+					console.log(err);
+					throw err;
+				}
+				var result = parseHeaderFile(data);
+				out[result["type"]] = result;
+				if (fileName == files[files.length - 1]) {
+					var strOut = "var temp = " + JSON.stringify(out, null, "\t") + ";"
+					fs.writeFile(outputFile, strOut, function (err) {
+						if (err) {
+							return console.log(err);
+						}
+						console.log("The file '" + outputFile + "' was saved!");
+					});
+				}
+			});
+		});
+	});
+}
+
+function getFunctions(data) {
+
+	var collect = "";
+	var count = 0;
+	var first = true;
+	var result = [];
+	var idx = 0;
+	var len = data.length;
+	for (var i = 0; i < len; i++) {
+		collect += data[i];
+		if (data[i] == "{") { count++; }
+		if (data[i] == "}") { count--; }
+		if (count > 0) { first = false; }
+		if (count == 0) {
+			if (!first || (data[i] == ";")) {
+				result[idx++] = collect;
+				collect = "";
+				first = true;
+			}
+		}
+	}
+	return result;
+}
+
+function removeCurlyCode(data) {
+	var collect = "";
+	var curlys = ["{","}"];
+	var count = 0;
+	var len = data.length;
+	for (var i = 0; i < len; i++) {
+		var char = data[i];
+		if (char == "{") { count++; }
+		if (!(char in curlys) && (count === 0)) {
+			collect += char;
+		}
+		if (char == "}") { count--; }
+	}
+	return collect;
+}
+
+function parseHeaderFile(data) {
+	var types = ["void", "float", "int16_t", "uint16_t", "uint32_t", "bool", "virtual"];
+	if (typeof data !== "string") {
+		return { "empty": { "type": "empty" }};
+	}
+	var lines = data.split("\n");
+	var lineBuff = "";
+	var result = {  "type": "empty",
+					"data": {
+						"defaults":  {
+							"name":      {
+								"value": "empty"
+							},
+							"getSource": {
+								"value": null
+							}
+						},
+						"shortName": "empty",
+						"inputs":    0,
+						"outputs":   0,
+						"category":  "empty",
+						"color":     "#E6E0F8",
+						"icon":      "arrow-in.png"
+					}
+				};
+	var active = false;
+	var skipRest = false;
+	var skipLine = false;
+	var complete = false;
+	var comment = 0;
+	var curlies = 0;
+	var comCount = 0;
+	var curlCount = false;
+	for (var i = 0; i < lines.length; i++) {
+		var line = lines[i].trim();
+		comment += (line.match(/\/\*/g) || []).length - (line.match(/\*\//g) || []).length;
+		if (comment > 0 || comCount > 0) {
+			comCount = comment ? comCount + 1 : 0;
+			continue;
+		}
+		if (curlCount) {
+			curlies += (line.match(/\{/g) || []).length - (line.match(/\}/g) || []).length;
+			curlCount = curlies > 0;
+			continue;
+		}
+		var start = line.firstWord();
+		var matches = [];
+		switch (start) {
+			case result.type:
+				curlies += (line.match(/\{/g) || []).length - (line.match(/\}/g) || []).length;
+				curlCount = true;
+				break;
+			case "#define":
+			case "#ifndef":
+			case "#include":
+			case "extern":
+			case "//":
+			case "":
+				skipLine = true;
+				break;
+			case "class":
+				matches = line.match(/(^class\s*)([^ ]*)(.*$)/);
+				if (matches) {
+					result["type"] = matches[2].trim();
+				}
+				break;
+			case "public:":
+				active = true;
+				break;
+			case "private:":
+				active = false;
+				skipRest = true;
+				break;
+			default:
+				if (active) {
+					lineBuff += line;
+				}
+				break;
+		}
+		if (skipRest) {
+			var funcs = getFunctions(lineBuff);
+			len = funcs.length;
+			for (var j = 0; j < len; j++) {
+				var fnc = funcs[j];
+				if (fnc.indexOf(result["type"]) === 0) {
+					funcs[j] = "";
+					continue;
+				}
+				var rest = removeCurlyCode(fnc).trim();
+				var braces = rest.match(/(\([^\)]*\))/g);
+				braces = braces ? braces[0] : "";
+				rest = rest.replace(braces, "").trim();
+				var parts = rest.split(/\s/);
+				var dataName = "";
+				if (parts) {
+					if (parts[0].trim() !== result["type"]) {
+						dataName = parts[parts.length - 1];
+						params = braces.replace("(", "").replace(")", "").split(",");
+					}
+				}
+				len = params.length;
+				for (var x = 0; x < len; x++) {
+					var par = params[x].split(" ");
+					result["data"]["defaults"][dataName] = {"value": "0" };
+				}
+			}
+			break;
+		}
+	}
+	return result;
+}
 
 function checkPath(path, doCreate) {
 	try {
 		var test = fs.statSync(path).isDirectory();
 		if (!test && doCreate) {
-			fs.mkdirSync(path, 0o755);
+			fs.mkdirSync(path, 0x755);
 			return checkPath(path, false);
 		}
 	} catch (err) {
@@ -29,21 +207,10 @@ function checkPath(path, doCreate) {
 	}
 }
 
-function extractScripts(data) {
-	var $ = cheerio.load(data);
-	$('script').each(function(i, element){
-		var name = $(this).attr(tagAttr);
-		if (name) {
-			var fileName = outputPath + name + ".html";
-			fs.writeFile(fileName, $(this).text(), function (err) {
-				if (err) {
-					return console.log(err);
-				}
-				console.log("The file '" + fileName + "' was saved!");
-			});
-		}
-	});
+String.prototype.firstWord = function() {
+	var split = this.split(/\s/);
+	result = (split) ? split[0] : this;
+	return result;
 }
-
 
 
